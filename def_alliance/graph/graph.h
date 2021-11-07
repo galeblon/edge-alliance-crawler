95a00ebe58b31e5ed40eb93312b6f6f1bb42f411
Diamond Dust <dziecim@o2.pl>
2021-11-07 17:07:37 +0100

    graph module now follows the pointer-only paradigm. Vertices array transformed into a linked list. Vertices idx now an explicit id. Simple id allocation strategy implemented (tally counter). Few basic graph functionalities added.

--- def_alliance/graph/graph.h
+++ def_alliance/graph/graph.h
@@ -1,24 +1,41 @@
 struct edge {
+    // Next edge in the Linked List
     struct edge * next;
-    uint64_t to;
+    // Pointer to the vertex that the edge is leading to
+    struct vertex * to;
 };
 
 struct vertex {
-    uint64_t idx;
+    // Vertex id in its graph
+    uint64_t id;
+    // Linked List of vertices
+    struct vertex * next;
+    // Linked List of edges from this vertex
     struct edge * edge;
 };
 
 struct graph {
+    // Number of vertices in a graph
     uint64_t n;
+    // Id counter for the vertices
+    // Notice that this simple id algorithm is the limit on vertex operations
+    uint64_t id_tally;
+    // Linked List of vertices
     struct vertex * v;
 };
 
 struct graph new_graph(uint64_t nvertices);
 struct graph parse_graph6(const char * buff);
-void add_edge(struct graph * g, uint64_t from, uint64_t to);
-struct vertex * get_vertex(struct graph * g, uint64_t index);
+void add_edge(struct vertex * from, struct vertex * to);
+struct vertex * get_vertex(struct graph * g, uint64_t id);
 void delete_graph(struct graph * g);
 void print_graph(struct graph * g);
 
-
-
+void delete_edge(struct vertex * from, struct vertex * to);
+uint64_t get_indegree(struct vertex * v);
+uint64_t get_outdegree(struct vertex * v);
+uint64_t get_degree(struct vertex* v);
+uint64_t get_degree(struct graph* g);
+bool is_balanced(struct graph * g);
+struct vertex * add_vertex(struct graph * g);
+void delete_vertex(struct graph* g, struct vertex* v);
