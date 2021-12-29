9a8ee4dc656ff3d1cd938560af80db0e0694cce4
Adrian <misiakadrian@onet.eu>
2021-12-29 14:59:23 +0100

    Fix for skipping header in csv file

--- utils/vertex_mapper.py
+++ utils/vertex_mapper.py
@@ -6,7 +6,7 @@ if len(sys.argv) < 3:
     print("<Community .csv file> <.json file>")
     exit(1)
 
-community = list((pd.read_csv(sys.argv[1]))['0'])
+community = list((pd.read_csv(sys.argv[1], usecols=[0], names=['0']))['0'])
 raw = pd.read_json(sys.argv[2])
 meta = raw.meta
 
